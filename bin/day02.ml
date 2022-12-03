065e8dfe25063eafbb15d78df68d9a4a3e250e2b
Jackson Riley <jackson.riley@btinternet.com>
2022-12-03 11:21:44 +0000

    Day 2 part 2

--- bin/day02.ml
+++ bin/day02.ml
@@ -4,33 +4,70 @@ open List
 open String
 
 type throw = Rock | Paper | Scissors
-type game = throw * throw
+type aim = Win | Lose | Draw
+type game1 = throw * throw
+type game2 = throw * aim
 
 let input : string list = read_whole_file "input/02" |> String.split ~on:'\n'
 
-let make_throw (c : char) =
+let make_throw (c : char) : throw =
   match c with
   | 'A' | 'X' -> Rock
   | 'B' | 'Y' -> Paper
   | 'C' | 'Z' -> Scissors
   | _ -> Printf.sprintf "Unrecognised character: '%c'!" c |> failwith
 
-let make_game (l : string) =
+let make_aim (c : char) : aim =
+  match c with
+  | 'X' -> Lose
+  | 'Y' -> Draw
+  | 'Z' -> Win
+  | _ -> Printf.sprintf "Unrecognised character: '%c'!" c |> failwith
+
+let make_game1 (l : string) : game1 =
   match explode_string l with
   | [ them; ' '; us ] -> (make_throw them, make_throw us)
   | _ -> failwith "Unrecognised game!"
 
-let shape_score (_, us) = match us with Rock -> 1 | Paper -> 2 | Scissors -> 3
+let make_game2 (l : string) : game2 =
+  match explode_string l with
+  | [ them; ' '; a ] -> (make_throw them, make_aim a)
+  | _ -> failwith "Unrecognised game!"
 
-let game_score game =
+let shape_score (t : throw) : int =
+  match t with Rock -> 1 | Paper -> 2 | Scissors -> 3
+
+let winner (t : throw) : throw =
+  match t with Rock -> Paper | Paper -> Scissors | Scissors -> Rock
+
+let loser (t : throw) : throw =
+  match t with Rock -> Scissors | Paper -> Rock | Scissors -> Paper
+
+let calculate_our_throw ((their_throw, aim) : game2) : throw =
+  match aim with
+  | Draw -> their_throw
+  | Win -> winner their_throw
+  | Lose -> loser their_throw
+
+let game1_score ((_, our_throw) as game : game1) : int =
+  shape_score our_throw
+  +
   match game with
   | Rock, Rock | Paper, Paper | Scissors, Scissors -> 3
   | Rock, Paper | Paper, Scissors | Scissors, Rock -> 6
   | _ -> 0
 
-let part1 =
-  List.map ~f:make_game input
-  |> List.map ~f:(fun g -> shape_score g + game_score g)
+let game2_score ((their_throw, _) as game : game2) : int =
+  let our_throw = calculate_our_throw game in
+  game1_score (their_throw, our_throw)
+
+let calculate_part maker scorer input = 
+  List.map ~f:maker input
+  |> List.map ~f:scorer
   |> List.fold_left ~f:( + ) ~init:0
 
+let part1 : int = calculate_part make_game1 game1_score input
+let part2 : int = calculate_part make_game2 game2_score input
+
 let () = Stdio.print_endline ("Part 1: " ^ Int.to_string part1)
+let () = Stdio.print_endline ("Part 2: " ^ Int.to_string part2)
